0fbd04ecd37f23089c2bc0ca504b76e42c7a002d
Vamsi Krishna Dasari <dasarivamsikrishna07@gmail.com>
2025-07-19 13:17:43 +0530

    Initial commit for Block Puzzle Game

--- src/audio.py
+++ src/audio.py
@@ -2,16 +2,23 @@ import pygame
 
 class AudioManager:
     def __init__(self):
-        pygame.mixer.init()
-        self.sounds = {}
-        self.music_volume = 0.7
-        self.sfx_volume = 0.8
+        try:
+            pygame.mixer.init()
+            self.sounds = {}
+            self.music_volume = 0.7
+            self.sfx_volume = 0.8
+            self.enabled = True
+        except:
+            self.enabled = False
         
     def load_sounds(self):
-        # You'll need to add sound files to assets/sounds/
+        if not self.enabled:
+            return
+            
+        # For web deployment, sounds are optional
         sound_files = {
             'line_clear': 'line_clear.wav',
-            'block_drop': 'block_drop.wav',
+            'block_drop': 'block_drop.wav', 
             'game_over': 'game_over.wav',
             'level_up': 'level_up.wav'
         }
@@ -21,8 +28,11 @@ class AudioManager:
                 self.sounds[name] = pygame.mixer.Sound(f'assets/sounds/{file}')
                 self.sounds[name].set_volume(self.sfx_volume)
             except:
-                pass  # Sound file not found
+                pass  # Sound file not found - game works without audio
     
     def play_sound(self, sound_name):
-        if sound_name in self.sounds:
-            self.sounds[sound_name].play()
+        if self.enabled and sound_name in self.sounds:
+            try:
+                self.sounds[sound_name].play()
+            except:
+                pass  # Ignore audio errors in web environment
